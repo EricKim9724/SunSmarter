91995eb69e183a90ec231de80707bdccb2d4b890
victorwkb <victorwkb@gmail.com>
2024-03-11 14:10:41 +1100

    fix typo

--- utils/authentication.py
+++ utils/authentication.py
@@ -6,7 +6,7 @@ import os
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_DATABASE = os.getenv("DB_NAME")
+DB_DATABASE = os.getenv("DB_DATABASE")
 
 
 # Function to establish a connection to the MySQL database
